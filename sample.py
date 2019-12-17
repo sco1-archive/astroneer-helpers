cf0d2744e9797c40ecb2a9d1bc5e333518b525ee
S. Co1 <sco1.git@gmail.com>
2019-12-17 11:55:26 -0500

    Add samples

--- sample.py
+++ sample.py
@@ -1,15 +1,6 @@
-# Astroneer helpers
-
-Yay!
-
-## BOM
-Reduce the provided build list into a table of base resources
-
-```py
 from collections import defaultdict
 
 from bom import build_table
-
 
 ELA_ROVER = defaultdict(int)
 ELA_ROVER.update(
@@ -29,26 +20,3 @@ ELA_ROVER.update(
 )
 
 print(build_table(ELA_ROVER))
-```
-
-```
-ALUMINUM   5
-ARGON      2
-CARBON     9
-CERAMIC    6
-COMPOUND   4
-COPPER     5
-GLASS      2
-GRAPHITE   3
-HELIUM     1
-HYDROGEN   3
-IRON       2
-LITHIUM    1
-METHANE    2
-NITROGEN   3
-ORGANIC    4
-QUARTZ     2
-RESIN      8
-TITANIUM  16
-TUNGSTEN   9
-```
